0609a95529ecd06c24197789a7d0a161ce5dbc39
hjpvandijk <hugojpvandijk@gmail.com>
2021-06-03 18:15:50 +0200

    Moved logui configuration to Session.js of the async task. This is done so that eventlisteners are only mounted after all popups have occurred. Screenrecording also starts then, and now works on firefox as well.

--- src/js/app/tasks/example-group-async/Session.js
+++ src/js/app/tasks/example-group-async/Session.js
@@ -8,7 +8,138 @@ import IntroStore from "../../../stores/IntroStore";
 class Session extends React.PureComponent {
     componentDidMount() {
         IntroStore.startIntro(introSteps, () => {});
+
+        document.querySelector(".introjs-skipbutton").addEventListener("click", 
+        () => {
+                window.LogUI.startScreenCapture();
+                if (window.hasOwnProperty('LogUI')) {
+                    this.startLogUI();
+                };
+            }
+        );
+
+
+       
     }
+
+    startLogUI(){
+        let configurationObject = {
+            logUIConfiguration: {
+            endpoint: 'ws://localhost/ws/endpoint/',
+            authorisationToken: 'eyJ0eXBlIjoibG9nVUktYXV0aG9yaXNhdGlvbi1vYmplY3QiLCJhcHBsaWNhdGlvbklEIjoiYzRhOTVkYTItYjNjOS00OGMyLWJlMDMtYmQ4ZGZkMzUxY2ZiIiwiZmxpZ2h0SUQiOiI3YzllMzhhOS0wOTY0LTQ0MmYtYThjNy1jNmQ4YzhmMWQ0YTcifQ:1lg5HA:ySmnMRxx4LODDBcRRQgDWqZn8M8d_hEFW_9EoT3diSE',  // The authentication token.
+            verbose: true,
+            browserEvents: {
+                // See the Browser Events Wiki page.
+                eventsWhileScrolling: false,
+                URLChanges: false,
+                contextMenu: false,
+                pageFocus: false,
+                trackCursor: false,
+                pageResize: false
+            }
+            },
+            applicationSpecificData: {
+            },
+            trackingConfiguration: {
+            // 'querybox-focus': {
+            //     selector: '#input-box',
+            //     event: 'focus',
+            //     name: 'QUERYBOX_FOCUS',
+            // },
+            // 'querybox-losefocus': {
+            //     selector: '#input-box',
+            //     event: 'blur',
+            //     name: 'QUERYBOX_BLUR',
+            // },
+            // 'left-rail-item-mousemovements': {
+            //     selector: '#left-rail-results li',
+            //     event: 'mouseHover',
+            //     properties: {
+            //         mouseenter: {
+            //             name: 'LEFT_RAIL_RESULT_HOVER_IN',
+            //         },
+            //         mouseleave: {
+            //             name: 'LEFT_RAIL_RESULT_HOVER_OUT',
+            //         }
+            //     },
+            //     metadata: [
+            //         {
+            //             nameForLog: 'resultRank',
+            //             sourcer: 'elementAttribute',
+            //             lookFor: 'data-rank',
+            //         }
+            //     ]
+            // },
+                // 'querybox' : {
+                // 	selector: '.form-control',
+                // 	event: 'input',
+                // 	name: 'inputgrouptest',
+                // 	metadata: [
+                // 		{
+                // 			nameForLog: 'input value',
+                // 			sourcer: 'elementAttribute',
+                // 			lookFor: 'value',
+                // 		}
+                // 	]
+                // },
+                'mouseclick' : {
+                    selector: '#header',
+                    event: 'click',
+                    name: 'click-test',
+                    // properties: {
+                    // 	primary: {
+                    // 		name: 'MOUSE_CLICK',
+                    // 	},
+                    // 	secondary: {
+                    // 		name: 'MOUSE_CLICK',
+                    // 	},
+                    // },
+                },
+                'query-submission': {
+                    selector: '.form',
+                    event: 'formSubmission',
+                    name: 'QUERY_SUBMITTED2',
+                    properties: {
+                        includeValues: [
+                            {
+                                nameForLog: 'completeQuery',
+                                sourcer: 'elementAttribute',
+                                selector: '.form-control',
+                                lookFor: 'value',
+                            }
+                        ]
+                    }
+                }
+            
+            },
+        };
+
+
+        var prev = 0;
+        var observer = new MutationObserver(function (mutationRecords) {
+            if(document.getElementsByClassName("modal").length === 1 && prev === 0){
+                window.LogUI.logCustomMessage({
+                    name: 'MODAL_DIALOG_SHOW'
+                });
+                prev = 1;
+            } else if(document.getElementsByClassName("modal").length === 0 && prev === 1){
+                window.LogUI.logCustomMessage({
+                    name: 'MODAL_DIALOG_HIDE'
+                });
+                prev = 0;
+            };
+        });
+
+        observer.observe(document.querySelector(".SearchResultsContainer").firstElementChild, {subTree: true, childList: true});
+
+        document.onclick = function(){
+            window.LogUI.logCustomMessage({
+                    name: 'click'
+                });
+        };
+
+        window.LogUI.init(configurationObject);
+    };
 
     render() {
         const task = AccountStore.getTask();
